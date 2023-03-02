b3dc88a251ba9cafc631596811a1a19a0e91cd98
Juan René Hernández Sánchez <jhernandezs1509@alumno.ipn.mx>
2023-03-02 11:04:15 -0600

    update

---
+++ Clase06/ejerciciosSerie1.java
@@ -0,0 +1,8 @@
+public class ejerciciosSerie1 {
+    public static void main(String[] args) {
+        double c =20;
+        double f;
+        f=((float)9/(float)5)*c+32.0;
+        System.out.println(f);
+    }
+}
